4b634d2603259f24d060c55077388ed42e4854f8
Kobina Otchere <kobinaotchere@Kobinas-MacBook-Pro.local>
2025-09-10 11:31:42 +0000

    count down init

---
+++ index.js
@@ -0,0 +1,18 @@
+const header = document.getElementById('countdown');
+const button = document.getElementById('startButton');
+let startTime = 10;
+
+header.innerHTML = `Start time: ${startTime}`;
+
+button.addEventListener('click', () => {
+    setInterval(() => {
+        if (startTime >= 0) {
+            header.innerHTML = `Start time: ${startTime}`;
+            startTime--;
+        }
+        else {
+            setTimeout(0);
+            header.innerHTML += '<p>Time up</p>'
+        }
+    }, 1000)
+});
