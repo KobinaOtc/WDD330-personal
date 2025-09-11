78a2da1affd381e9d473cbbd4f63c6f41047f85f
Kobina Otchere <kobinaotchere@Kobinas-MacBook-Pro.local>
2025-09-11 13:12:47 +0000

    Tests done

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const header = document.getElementById('countdown');
-const button = document.getElementById('startButton');
-let startTime = 10;
+// const header = document.getElementById('countdown');
+// const button = document.getElementById('startButton');
+// let startTime = 10;
 
-header.innerHTML = `Start time: ${startTime}`;
+// header.innerHTML = `Start time: ${startTime}`;
 
-button.addEventListener('click', () => {
-    setInterval(() => {
-        if (startTime >= 0) {
-            header.innerHTML = `Start time: ${startTime}`;
-            startTime--;
-        }
-        else {
-            setTimeout(0);
-            header.innerHTML += '<p>Time up</p>'
-        }
-    }, 1000)
-});
+// button.addEventListener('click', () => {
+//     setInterval(() => {
+//         if (startTime >= 0) {
+//             header.innerHTML = `Start time: ${startTime}`;
+//             startTime--;
+//         }
+//         else {
+//             setTimeout(0);
+//             header.innerHTML += '<p>Time up</p>'
+//         }
+//     }, 1000)
+// });
+
+let count = 0;
+const intervalId = setInterval(() => {
+  count += 1;
+  console.log(count);
+  if (count === 3) {
+    clearInterval(intervalId);
+  }
+}, 1000);
